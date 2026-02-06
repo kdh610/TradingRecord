34773feab256b03384b4d24adb19607f6388a5a3
kdh610 <k_dh610@naver.com>
2026-02-07 01:25:39 +0900

    first commit

---
+++ src/main/java/com/tradingRecord/tradingRecord/TradingRecordApplication.java
@@ -0,0 +1,13 @@
+package com.tradingRecord.tradingRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradingRecord/tradingRecord/TradingRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingRecord.tradingRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

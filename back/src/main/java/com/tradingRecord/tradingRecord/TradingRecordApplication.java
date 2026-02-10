77f18d2bae6260669913943186da11983ed54836
kdh <k_dh610@naver.com>
2026-02-11 01:02:59 +0900

    refactor: 백엔드 프로젝트를 back 폴더로 이동

--- back/src/main/java/com/tradingRecord/tradingRecord/TradingRecordApplication.java
+++ back/src/main/java/com/tradingRecord/tradingRecord/TradingRecordApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TradingRecordApplication {
+public class
+TradingRecordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingRecordApplication.class, args);

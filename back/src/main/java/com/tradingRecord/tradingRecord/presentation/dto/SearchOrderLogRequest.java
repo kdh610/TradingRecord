cc06907f2b7c2ea461a788eb21f9e13c55b7db22
kdh <k_dh610@naver.com>
2026-02-13 14:52:56 +0900

    refactor: endpoint이름 변경

--- back/src/main/java/com/tradingRecord/tradingRecord/presentation/dto/SearchOrderLogRequest.java
+++ back/src/main/java/com/tradingRecord/tradingRecord/presentation/dto/SearchOrderLogRequest.java
@@ -1,16 +1,15 @@
 package com.tradingRecord.tradingRecord.presentation.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 public record SearchOrderLogRequest(
-        @JsonProperty("stk_nm")String stkNm,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-        @JsonProperty("start") LocalDate start,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-        @JsonProperty("end") LocalDate end
+        String stkNm,
+        @DateTimeFormat(pattern = "yyyyMMdd")
+        LocalDate start,
+        @DateTimeFormat(pattern = "yyyyMMdd")
+        LocalDate end
 
 ) {
 }
